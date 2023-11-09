7aef65839ff3d82b803deb520591da67a883d936
RufusCool <msrampasso@gmail.com>
2023-11-09 15:42:35 -0500

    main

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 							align-items: center;
 							height: 100vh;
 							background-color: #00FF00; /* Cor de fundo verde claro */
+							flex-direction: column;
 						}
 						h1 {
 							text-align: center;
@@ -29,17 +30,14 @@ func main() {
 						p {
 							text-align: center;
 							color: #333; /* Defina a cor do texto que desejar */
-							display: flex;
-							flex-direction: column;
 						}
 					</style>
 				</head>
 				<body>
 					<h1>Workshop DevOps !!!!</h1>
-					<p>
-						<span>Nome do Pod:</span>
-						<span>` + hostname + `</span>
-					</p>
+					<div style="display: flex; flex-direction: column; align-items: center;">
+						<p>Nome do Pode: ` + hostname + `</p>
+					</div>
 				</body>
 			</html>
 		`))
